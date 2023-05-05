37b3ad7c3c08cec937f15c549b0e359cdccc9678
Reda Ali <reda5201028@gmail.com>
2023-05-05 16:12:22 +0300

    Config and Database

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database
+{
+    public $connection;
+
+    public function __construct($config, $username, $password)
+    {
+        $dsn = "mysql:" . http_build_query($config, '', ';');
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($statement, $params = [])
+    {
+        $statement = $this->connection->prepare($statement);
+        $statement->execute($params);
+
+        return $statement;
+    }
+}
